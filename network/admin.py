49a40e5e624a09fb91cbf4a32f965174c032bc7a
CptCaveira <janos.rocha@gmail.com>
2021-05-05 10:47:34 +0200

    First Migration - Model Creation

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User, Post, Comments, Likes, Following 
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Post)
+admin.site.register(Comments)
+admin.site.register(Likes)
+admin.site.register(Following)
--- network/models.py
+++ network/models.py
@@ -1,6 +1,31 @@
 from django.contrib.auth.models import AbstractUser
+from django.db.models.fields.related import ForeignKey, ManyToManyField
 from django.db import models
 
 
 class User(AbstractUser):
     pass
+
+
+class Post(models.Model):
+    post = models.TextField(max_length=500)
+    added = models.DateField(auto_now_add=True)
+    
+
+class Comments(models.Model):
+    class Meta:
+        verbose_name_plural = "comments"
+    user = ForeignKey(User, on_delete=models.PROTECT, related_name="comments")
+    post = ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    comment = models.TextField(max_length=750)
+    date_added = models.DateField(auto_now_add=True)
+
+class Likes(models.Model):
+    class Meta:
+        verbose_name_plural = "likes"
+    likes = models.IntegerField
+    post = ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
+    user = ForeignKey(User, on_delete=models.PROTECT, related_name="likes")
+
+class Following(models.Model):
+    follow = ManyToManyField(User, related_name="following")
